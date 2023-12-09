c3a26cd969f3225f486ad93f332371681e113c39
Konstantin Rybakov <i@krybakov.com>
2023-12-09 13:43:54 +0200

    skip pragmatic engineer for now

--- lib/job-search/check-jobs.ts
+++ lib/job-search/check-jobs.ts
@@ -1,14 +1,14 @@
 import { sendMessage } from '../echobot/send-message.ts'
 import { log } from '../log.ts'
 import { bufferChain } from './company-chains/buffer.ts'
-import { pragmaticEngineerChain } from './company-chains/pragmatic-engineer.ts'
+// import { pragmaticEngineerChain } from './company-chains/pragmatic-engineer.ts'
 import { zapierChain } from './company-chains/zapier.ts'
 import { createJobMessage } from './messaging/create-job-message.ts'
 
 export const checkJobs = async () => {
   log.info('Checking jobs')
 
-  const companies = [pragmaticEngineerChain, bufferChain, zapierChain]
+  const companies = [/* pragmaticEngineerChain */ bufferChain, zapierChain]
 
   const jobs = await Promise.all(companies.map(company => company()))
 
