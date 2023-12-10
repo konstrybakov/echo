3404a68e6e89848f3909f056e39517a9e2c8f12f
Konstantin Rybakov <i@krybakov.com>
2023-12-10 16:48:05 +0200

    uncomment the other companies

--- lib/job-search/chain-map.ts
+++ lib/job-search/chain-map.ts
@@ -1,10 +1,10 @@
 export const companyChainMap = {
-  // DuckDuckGo: () => import('~/job-search/company-chains/duck-duck-go.ts'),
-  // PragmaticEngineer: () => import('~/job-search/company-chains/pragmatic-engineer.ts'),
-  // Buffer: () => import('~/job-search/company-chains/buffer.ts'),
-  // Zapier: () => import('~/job-search/company-chains/zapier.ts'),
-  // Memorisely: () => import('~/job-search/company-chains/memorisely.ts'),
-  // Splice: () => import('~/job-search/company-chains/splice.ts'),
+  DuckDuckGo: () => import('~/job-search/company-chains/duck-duck-go.ts'),
+  PragmaticEngineer: () => import('~/job-search/company-chains/pragmatic-engineer.ts'),
+  Buffer: () => import('~/job-search/company-chains/buffer.ts'),
+  Zapier: () => import('~/job-search/company-chains/zapier.ts'),
+  Memorisely: () => import('~/job-search/company-chains/memorisely.ts'),
+  Splice: () => import('~/job-search/company-chains/splice.ts'),
   OnDeck: () => import('~/job-search/company-chains/on-deck.ts')
 }
 
